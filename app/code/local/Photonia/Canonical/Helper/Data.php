b1a868681a45976f312455deeffd5a043093b28c
morphalex90 <piero.nanni@gmail.com>
2018-10-25 13:20:44 +0100

    New function with $_product parameter

--- app/code/local/Photonia/Canonical/Helper/Data.php
+++ app/code/local/Photonia/Canonical/Helper/Data.php
@@ -18,4 +18,19 @@ class Photonia_Canonical_Helper_Data extends Mage_Core_Helper_Abstract {
 		
         return $base_url.$url;
 	}
+	
+	public function getFullProductUrl($_product){
+		
+		$base_url = Mage::getBaseUrl(); // base url
+		
+		$categoryIds = $_product->getCategoryIds(); // load all categories
+        if (isset($categoryIds[0])){ // if at least 1 category exist
+            $category = Mage::getModel('catalog/category')->load($categoryIds[0]); // load the first category
+            $url = str_replace('.html','',$category->getUrlPath()).'/'.$_product->getUrlPath(); // generate the url with the category url
+        } else {
+            $url = $_product->getUrlPath(); // the product has no category, generate the url without product category
+        }
+		
+        return $base_url.$url;
+	}
 }
